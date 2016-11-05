47b7fe7290dd40793abb3824c851a2f13e4f2161
Matt Russell <matt.russell@nyu.edu>
2016-11-05 14:22:09 -0700

    Add files via upload

---
+++ urllink2.py
@@ -0,0 +1,19 @@
+# Note - this code must run in Python 2.x and you must download
+# http://www.pythonlearn.com/code/BeautifulSoup.py
+# Into the same folder as this program
+
+import urllib
+from BeautifulSoup import *
+
+url = raw_input('Enter - ')
+html = urllib.urlopen(url).read()
+
+soup = BeautifulSoup(html)
+
+# Retrieve all of the anchor tags
+tags = soup('span')
+sumit = 0
+for tag in tags:
+    # Look at the parts of a tag
+    sumit = int(tag.contents[0]) + sumit
+print sumit
